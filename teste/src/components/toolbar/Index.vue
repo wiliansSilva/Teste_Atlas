e2af37482b3d1759fcac1cc10f431372fb86c399
wilians <wdonizete.one@gmail.com>
2021-02-24 16:30:39 -0300

    toolbar for big screen ready

--- teste/src/components/toolbar/Index.vue
+++ teste/src/components/toolbar/Index.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="containerToolbar">
     <img id="img" src="../../assets/back.png">
+    <div class="containerLabelLogo">
+      <img id="imgLogo" src="../../assets/logo.png">
+      <h1 id="labelGit">GitSearch</h1>
+    </div>
     <h2 id="label">Lista de Usuários</h2>
-    
   </div>
 </template>
 
